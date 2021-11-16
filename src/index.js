1220cbe2c87a56e39ec0b5443bebf13db4d1c4f6
Pallavi Aggarwal <pallaviaggarwal28@gmail.com>
2021-11-16 22:08:38 +0100

    added port for listening

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./schema');
-const resolvers = require('./resolvers');
-const TrackAPI = require('./datasources/track-api');
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const TrackAPI = require("./datasources/track-api");
 
 async function startApolloServer(typeDefs, resolvers) {
   const server = new ApolloServer({
@@ -9,12 +9,12 @@ async function startApolloServer(typeDefs, resolvers) {
     resolvers,
     dataSources: () => {
       return {
-        trackAPI: new TrackAPI(),
+        trackAPI: new TrackAPI()
       };
-    },
+    }
   });
 
-  const { url, port } = await server.listen();
+  const { url, port } = await server.listen({ port: process.env.PORT || 4000 });
   console.log(`
       🚀  Server is running
       🔉  Listening on port ${port}
